b13fd4512f922b060ad01d1be855d9ba600adefd
Doncho <doncho.dobrev@gmail.com>
2017-09-21 20:43:45 +0300

    half-way homepage

--- project/assets/javascript/script.js
+++ project/assets/javascript/script.js
@@ -1,1 +1,10 @@
-console.log();
+$(document).ready(function(){
+
+$(document).scroll(function() { 
+    if($(window).scrollTop() !== 0) {
+      $("#header").css('opacity', 1);
+    }else{
+        $("header").css('opacity', 0.5); 
+    }
+ });
+});
