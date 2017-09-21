bdfb750325039572e6b027075eb37baa1f274c84
Doncho <doncho.dobrev@gmail.com>
2017-09-21 22:25:07 +0300

    half-way homepage

--- project/assets/javascript/script.js
+++ project/assets/javascript/script.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
-    $(document).scroll(function () {
-        if ($(window).scrollTop() !== 0) {
-            $("#header").css('background-color', 'white');
-        } else {
-            $("#header").css('background-color', 'blue');
-        }
-    });
+$(document).scroll(function() { 
+    if($(window).scrollTop() !== 0) {
+        $("#header_content").css('background-color', 'white');
+    }else{
+        $("#header_content").css('background-color', 'transparent');
+    }
+ });
 });
