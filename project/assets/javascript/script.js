1d22458443ff864ad06ccea2dffa288b090948a1
Doncho <doncho.dobrev@gmail.com>
2017-09-22 16:38:41 +0300

    Terms and conditions+ map added

--- project/assets/javascript/script.js
+++ project/assets/javascript/script.js
@@ -1,10 +1,44 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$(document).scroll(function() { 
-    if($(window).scrollTop() !== 0) {
-        $("#header_content").css('background-color', 'white');
-    }else{
-        $("#header_content").css('background-color', 'transparent');
-    }
- });
+    $(document).scroll(function () {
+        if ($(window).scrollTop() !== 0) {
+            $("#header_content").css('background-color', 'white');
+        } else {
+            $("#header_content").css('background-color', 'transparent');
+        }
+    });
+
+    $('#loginButton').on("click", function () {
+        $('#myModal').modal('toggle');
+        $('#register').css('visible', 'hidden');
+
+    });
+
+    $('#registerButton').on("click", function () {
+        $('#myModal2').modal('toggle');
+
+
+    });
+
+
+
+
+
+
 });
+
+
+function myMap() {
+    var myLatLng = { lat: 42.6643557, lng: 23.2879690 };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Pizza is here!'
+    });
+}
