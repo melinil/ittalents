56c69e212eecb3ccbd784f7cc70b8b3284836542
mastravchiyska <tatyana.mastravchiyska@gmail.com>
2017-09-24 20:41:33 +0300

    revert to old commit

--- project/assets/javascript/script.js
+++ project/assets/javascript/script.js
@@ -47,53 +47,40 @@ $(document).ready(function () {
         type: "POST",
         url: "products.json",
         success: function (data) {
-<<<<<<< HEAD
             data.deals.forEach(function (element) {
                 $("#products").append("<div class='menu-items'><img width='150px' src='" + element.image + "'><h3>" + element.name + "</h3>" +
                     "<div>Price: " + element.price + "</div><button class='btn btn-success'>CHOOSE</button></div>")
             });
-=======
->>>>>>> 6bd113707f8e02651a1f26cd4be35c79705cfb2e
             $("body").click(function (e) {
                 $("#products").empty();
                 var idClicked = e.target.id;
-                var print = function(element){
-                     $("#products").append("<div class='menu-items'><img width='150px' src='" + element.image + "'><h2>" + element.name + "</h2>"+
-                        "<div><h3>Price:</h3>"+element.price+ "</div><button class='btn btn-success'>CHOOSE</button></div>")
+                var print = function (element) {
+                    $("#products").append("<div class='menu-items'><img width='150px' src='" + element.image + "'><h2>" + element.name + "</h2>" +
+                        "<div><h3>Price:</h3>" + element.price + "</div><button class='btn btn-success'>CHOOSE</button></div>")
                 }
                 if (idClicked == "dealTab") {
                     data.deals.forEach(function (element) {
-<<<<<<< HEAD
-                        $("#products").append("<div class='menu-items'><img width='150px' src='" + element.image + "'><h3>" + element.name + "</h3>" +
-                            "<div>Price: " + element.price + "</div><button class='btn btn-success'>CHOOSE</button></div>")
-=======
-                       print(element);
->>>>>>> 6bd113707f8e02651a1f26cd4be35c79705cfb2e
+                        print(element);
                     });
                 }
                 if ((idClicked == "pizzaTab") || (idClicked == "menuButton")) {
                     data.pizzas.forEach(function (element) {
-<<<<<<< HEAD
-                        $("#products").append("<div class='menu-items'><img width='150px' src='" + element.image + "'><h3>" + element.name + "</h3>" +
-                            "<div>Price: " + element.price + "</div><button class='btn btn-success'>CHOOSE</button></div>")
-=======
                         print(element);
->>>>>>> 6bd113707f8e02651a1f26cd4be35c79705cfb2e
                     });
                 }
                 if (idClicked == "chickenTab") {
                     data.chicken.forEach(function (element) {
-                         print(element);
+                        print(element);
                     });
                 }
                 if (idClicked == "pastaTab") {
                     data.pasta.forEach(function (element) {
-                         print(element);
+                        print(element);
                     });
                 }
                 if (idClicked == "sandwichTab") {
                     data.sandwiches.forEach(function (element) {
-                         print(element);
+                        print(element);
                     });
                 }
                 if (idClicked == "souceTab") {
@@ -103,20 +90,17 @@ $(document).ready(function () {
                 }
                 if (idClicked == "desertTab") {
                     data.deserts.forEach(function (element) {
-                         print(element);
+                        print(element);
                     });
                 }
                 if (idClicked == "drinkTab") {
                     data.drinks.forEach(function (element) {
-                         print(element);
+                        print(element);
                     });
                 }
-<<<<<<< HEAD
                 else {
                     console.log("Hello!")
                 }
-=======
->>>>>>> 6bd113707f8e02651a1f26cd4be35c79705cfb2e
             });
         }
     });
